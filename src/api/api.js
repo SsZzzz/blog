5e9feb5d74b2ce2785196e92096bfe5ee717b5d8
Mendia <suze0526@gmail.com>
2019-04-08 20:50:01 +0800

    路由增加根路径

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 
-const getArticleList = (params) => { return axios.get('/blog/getArticleList', { params }) };
-const getArticle = (params) => { return axios.get('/blog/getArticle', { params }) };
-const getLabel = () => { return axios.get('/management/getLabel') };
-const getTimeLine = () => { return axios.get('/blog/getTimeLine') };
-const captcha = () => { return axios.get('/blog/captcha') };
-const register = (params) => { return axios.post('/blog/register', params) };
-const login = (params) => { return axios.post('/blog/login', params) };
+const getArticleList = (params) => { return axios.get('/blog-serve/blog/getArticleList', { params }) };
+const getArticle = (params) => { return axios.get('/blog-serve/blog/getArticle', { params }) };
+const getLabel = () => { return axios.get('/blog-serve/management/getLabel') };
+const getTimeLine = () => { return axios.get('/blog-serve/blog/getTimeLine') };
+const captcha = () => { return axios.get('/blog-serve/blog/captcha') };
+const register = (params) => { return axios.post('/blog-serve/blog/register', params) };
+const login = (params) => { return axios.post('/blog-serve/blog/login', params) };
 const saveImage = (params) => {
-  return axios.post('/blog/saveImage', params, {
+  return axios.post('/blog-serve/blog/saveImage', params, {
     headers: { 'Content-Type': 'multipart/form-data' }
   })
 };
-const editUserInfo = (params) => { return axios.post('/blog/editUserInfo', params) };
-const comment = (params) => { return axios.post('/blog/comment', params) };
-const getCommentList = (params) => { return axios.get('/blog/getCommentList', { params }) };
-const replyComment = (params) => { return axios.post('/blog/replyComment', params) };
+const editUserInfo = (params) => { return axios.post('/blog-serve/blog/editUserInfo', params) };
+const comment = (params) => { return axios.post('/blog-serve/blog/comment', params) };
+const getCommentList = (params) => { return axios.get('/blog-serve/blog/getCommentList', { params }) };
+const replyComment = (params) => { return axios.post('/blog-serve/blog/replyComment', params) };
 
 export default {
   getArticleList,
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	publicPath: '/blog/',
 	devServer: {
 		disableHostCheck: true,//内网穿透时得加上这个
-		proxy:'http://192.168.1.196:3001',
+		proxy:'http://192.168.3.9:3001',
 		port:9002
 	}
 }
